a69e63d7155c5afebba161a82462b3ee6b0da647
978387805 <978387805@qq.com>
2022-07-11 16:34:20 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,34 +1,61 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOBJ.h>
+#include <igl/png/readPNG.h>
+#include <igl/png/writePNG.h>
+#include <iostream>
 
-int main(int argc, char *argv[])
+
+//   V  double matrix of vertex positions  #V by 3
+//   TC  double matrix of texture coordinats #TC by 2
+//   N  double matrix of corner normals #N by 3
+//   F  #F list of face indices into vertex positions
+//   FTC  #F list of face indices into vertex texture coordinates
+//   FN  #F list of face indices into vertex normals
+Eigen::MatrixXd V;
+Eigen::MatrixXd TC;
+Eigen::MatrixXd N;
+Eigen::MatrixXi F;
+Eigen::MatrixXi FTC;
+Eigen::MatrixXi FN;
+
+bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+	if (key == '1')
+	{
+		// Allocate temporary buffers
+		Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> R(1280, 800);
+		Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> G(1280, 800);
+		Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> B(1280, 800);
+		Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> A(1280, 800);
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+		// Draw the scene in the buffers
+		viewer.core().draw_buffer(
+			viewer.data(), false, R, G, B, A);
+
+		// Save it to a PNG
+		igl::png::writePNG(R, G, B, A, "./Resources/out.png");
+	}
+	return false;
+}
+
+int main(int argc, char* argv[])
+{
+
+	//igl::read_triangle_mesh("./Resources/t1.obj", V, F);
+	igl::readOBJ("./Resources/t1.obj", V, TC, N, F, FTC, FN);
+	Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> R, G, B, A;
+	igl::png::readPNG("./Resources/t1_low.png", R, G, B, A);
+
+
+	// Plot the mesh
+	igl::opengl::glfw::Viewer viewer;
+	viewer.callback_key_down = &key_down;
+	viewer.data().set_mesh(V, F);
+	viewer.data().set_uv(TC, FTC);
+	viewer.data().set_texture(R, G, B, A);
+	viewer.data().show_lines = false;
+	viewer.data().show_texture = true;
+	viewer.core().orthographic = true;
+	// Launch the viewer
+	viewer.launch();
 }
